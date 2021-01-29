7e4e391be5c207e4545dca1278b9feba305d7721
cmarc <40406039+crrmarchese@users.noreply.github.com>
2021-01-28 23:13:47 -0500

    Code for 5 Day Forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,19 @@
 $(document).ready(function(){
 // Get current month, date and day and set the date format
-const currentMonth = (dayjs().format("MM"));
+//const currentMonth = (dayjs().format("MM"));
 // const dayOfWeek = (dayjs().format("dddd"));
-const dayOfMonth = (dayjs().format("D"));
-const currentYear = (dayjs().format("YYYY"));
+//const dayOfMonth = (dayjs().format("D"));
+//const currentYear = (dayjs().format("YYYY"));
+
+//Reference https://zetcode.com/javascript/dayjs/
+let now = dayjs();
+let currentDayVal = (now.format("MM/DD/YYYY"));
 
 
 // Return the current day, month and day 
-$("#currentDay").text(currentMonth + "/" + dayOfMonth + "/" + currentYear);
+$("#currentDay").text(currentDayVal);
 
+    //When the search button is clicked
     $("#button-search").click(function(){
         
         var weatherCity ="https://api.openweathermap.org/data/2.5/weather";
@@ -17,7 +22,7 @@ $("#currentDay").text(currentMonth + "/" + dayOfMonth + "/" + currentYear);
         let city = $("#citySearch").val().trim();
         var APIKEY = "0cc6bbe69909a36c9e8389677b2668e8";
       
-  
+        // Get data from weather API call
         $.ajax({
             url: weatherCity + "?q=" + city + "&" + "APPID=" + APIKEY,
             method: "GET"
@@ -29,16 +34,17 @@ $("#currentDay").text(currentMonth + "/" + dayOfMonth + "/" + currentYear);
 
                 // Transfer content to HTML
                 $("#cityName").text(response.name);
-                $("#windSpeed").text("Wind Speed: " + response.wind.speed);
-                $("#humidity").text("Humidity: " + response.main.humidity);
+                $("#windSpeed").text("Wind Speed: " + response.wind.speed + " MPH");
+                $("#humidity").text("Humidity: " + response.main.humidity + "%");
                 $("#city-weather-icon").text();
                 
                 // Convert the temp to fahrenheit
                 var tempF = (response.main.temp - 273.15) * 1.80 + 32;
 
                 // add temp content to html
-                 $("#temperature").text("Temperature (F) " + tempF.toFixed(2));
-                // Get Lat/Long/Weather
+                 $("#temperature").text("Temperature: " + tempF.toFixed(1) + " °F");
+
+                // Get Lat/Long
                 let cityLat = response.coord.lat;
                 let cityLong = response.coord.lon;
 
@@ -65,7 +71,7 @@ $("#currentDay").text(currentMonth + "/" + dayOfMonth + "/" + currentYear);
                 } else if (weatherIconRes === "few clouds") {
                     $("#city-weather-icon img").attr("src", weatherIconURL + weatherIcons.fewClouds+ weatherIconURLSuffix);
                 } else if (weatherIconRes === "scattered clouds") {
-                    $("#city-weather-icon img").attr("src", weatherIconURL + weatherIcons.scatterdClouds + weatherIconURLSuffix);
+                    $("#city-weather-icon img").attr("src", weatherIconURL + weatherIcons.scatteredClouds + weatherIconURLSuffix);
                 } else if (weatherIconRes === "broken clouds") {
                     $("#city-weather-icon img").attr("src", weatherIconURL + weatherIcons.brokenClouds + weatherIconURLSuffix);
                 } else if (weatherIconRes === "shower rain") {
@@ -81,18 +87,71 @@ $("#currentDay").text(currentMonth + "/" + dayOfMonth + "/" + currentYear);
                 }
         
              
-                // Data call for UV Index
+                // Data call for UV Index (OneCall) using Lat/Long from weather API URL
                 $.ajax({
                     url: uvIndex + "?lat=" + cityLat + "&lon=" + cityLong + "&appid=" + APIKEY,
                     method: "GET"
                     }).then(function(response) {
-                         //console.log(response);
+                        // console.log(typeof response.daily[0].weather[0].icon);
                         $("#uvIndex").text("UV Index:");
                         $("#uvIndexNum").text(response.current.uvi);
                         
+                        
                         addClassUVIndex(); 
+
+
+                // Five Day Forecast Data
+                    // Day 1
+                    let forecastDay1Date = now.add('1', 'day');
+                    $(".day1-forecast-date").text(forecastDay1Date.format('MM/DD/YYYY'));
+                    $(".day1-forecast-icon").attr("src", weatherIconURL + response.daily[0].weather[0].icon + ".png");
+                    $(".day1-forecast-icon").attr("alt", response.daily[0].weather[0].description);
+                    let kelvinForecastDay1 = ((response.daily[0].temp.day - 273.15) * 1.80 + 32).toFixed(2);
+                    $(".day1-forecast-temp").text("Temp: " + kelvinForecastDay1 + " °F");
+                    $(".day1-forecast-humidity").text("Humidity: " + response.daily[0].humidity + "%");
+
+                     // Day 2
+                     let forecastDay2Date = now.add('2', 'day');
+                     $(".day2-forecast-date").text(forecastDay2Date.format('MM/DD/YYYY'));
+                     $(".day2-forecast-icon").attr("src", weatherIconURL + response.daily[1].weather[0].icon + ".png");
+                     $(".day2-forecast-icon").attr("alt", response.daily[1].weather[0].description);
+                     let kelvinForecastDay2 = ((response.daily[1].temp.day - 273.15) * 1.80 + 32).toFixed(2);
+                     $(".day2-forecast-temp").text("Temp: " + kelvinForecastDay2 + " °F");
+                     $(".day2-forecast-humidity").text("Humidity: " + response.daily[1].humidity + "%");
+
+                     // Day 3
+                     let forecastDay3Date = now.add('3', 'day');
+                     $(".day3-forecast-date").text(forecastDay3Date.format('MM/DD/YYYY'));
+                     $(".day3-forecast-icon").attr("src", weatherIconURL + response.daily[2].weather[0].icon + ".png");
+                     $(".day3-forecast-icon").attr("alt", response.daily[2].weather[0].description);
+                     let kelvinForecastDay3 = ((response.daily[2].temp.day - 273.15) * 1.80 + 32).toFixed(2);
+                     $(".day3-forecast-temp").text("Temp: " + kelvinForecastDay3 + " °F");
+                     $(".day3-forecast-humidity").text("Humidity: " + response.daily[2].humidity + "%");
+
+                     // Day 4
+                     let forecastDay4Date = now.add('4', 'day');
+                     $(".day4-forecast-date").text(forecastDay4Date.format('MM/DD/YYYY'));
+                     $(".day4-forecast-icon").attr("src", weatherIconURL + response.daily[3].weather[0].icon + ".png");
+                     $(".day4-forecast-icon").attr("alt", response.daily[3].weather[0].description);
+                     let kelvinForecastDay4 = ((response.daily[3].temp.day - 273.15) * 1.80 + 32).toFixed(2);
+                     $(".day4-forecast-temp").text("Temp: " + kelvinForecastDay4 + " °F");
+                     $(".day4-forecast-humidity").text("Humidity: " + response.daily[3].humidity + "%");
+
+                     // Day 5
+                     let forecastDay5Date = now.add('5', 'day');
+                     $(".day5-forecast-date").text(forecastDay5Date.format('MM/DD/YYYY'));
+                     $(".day5-forecast-icon").attr("src", weatherIconURL + response.daily[4].weather[0].icon + ".png");
+                     $(".day5-forecast-icon").attr("alt", response.daily[4].weather[0].description);
+                     let kelvinForecastDay5 = ((response.daily[4].temp.day - 273.15) * 1.80 + 32).toFixed(2);
+                     $(".day5-forecast-temp").text("Temp: " + kelvinForecastDay5 + " °F");
+                     $(".day5-forecast-humidity").text("Humidity: " + response.daily[4].humidity + "%");
+
+                   
                        
                 });
+
+
+
             });
         
     });
@@ -111,6 +170,8 @@ $("#currentDay").text(currentMonth + "/" + dayOfMonth + "/" + currentYear);
         } else {
             $("#uvIndexNum").addClass("uv-extreme");
         }
-    } 
+    }
+    
+   
 
 });
